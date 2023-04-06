ca10584f3ed5ec383c87b0da18fbda2859abd119
Philip Haggard <prh148@msstate.edu>
2023-04-05 20:11:34 -0500

    fix pylint issues

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
+# Importing Flask
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+# This function calculates the bmi value given the weight and height of the person in pounds
 def calculate_bmi_value(weight, height):
     bmi = round((weight * 0.45) / ((height * 0.025)*(height * 0.025)), 1)
     return bmi
 
+# This function calculates the bmi category given the bmi value
 def calculate_bmi_category(bmi):
     if bmi < 18.5:
         category = 'Underweight'
-    elif bmi >= 18.5 and bmi <= 24.9:
+    elif 18.5 <= bmi <= 24.9:
         category = 'Normal weight'
-    elif bmi >= 25 and bmi <= 29.9:
+    elif 25 <= bmi <= 29.9:
         category = 'Overweight'
     else:
         category = 'Obese'
     return category
 
+# Flask implementation
 @app.route('/', methods=['GET', 'POST'])
 def index():
     bmi = None
