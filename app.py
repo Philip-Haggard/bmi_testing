2217a4fa3b08e49dd9f13db83fb40857bddb2c02
Philip Haggard <prh148@msstate.edu>
2023-04-05 19:08:58 -0500

    refactor code and add flask implementation

--- app.py
+++ app.py
@@ -1,48 +1,33 @@
-##################################################
-#
-# This program allows a user to enter their
-# height and weight and will return their
-# BMI value and category
-#
-##################################################
+from flask import Flask, request, render_template
 
-class BMI:
+app = Flask(__name__)
 
-    def bmi_category_calculation(self, x):
-            if x < 18.5:
-                return "Underweight"
-            elif x >= 18.5 and x < 25:
-                return "Normal weight"
-            elif x >= 25 and x < 30:
-                return "Overweight"
-            else:
-                return "Obese"
-        
-    def bmi_value_calculation(self):
-        height = input("Please enter your height in feet and inches (example: 5 9 or 6 2): ")
-        height_ft, height_in, *_ = map(int, height.split())
-        weight = float(input("Please enter your weight in pounds: "))
+def calculate_bmi_value(weight, height):
+    bmi = round(((weight / (height * height)) * 703), 1)
+    return bmi
 
-        if height_ft < 0 or height_in < 0:
-            raise ValueError("Height must be positive!")
-        elif height_ft == 0 and height_in == 0:
-             raise ValueError("Height must be greater than zero!")
-        elif height_in > 11:
-             raise ValueError("Height in inches must be less than 12!")
-        elif weight < 0:
-             raise ValueError("Weight must be greater than zero!")
-        elif weight == 0:
-             raise ValueError("Weight must be greater than zero!")
+def calculate_bmi_category(bmi):
+    if bmi < 18.5:
+        category = 'Underweight'
+    elif bmi >= 18.5 and bmi <= 24.9:
+        category = 'Normal weight'
+    elif bmi >= 25 and bmi <= 29.9:
+        category = 'Overweight'
+    else:
+        category = 'Obese'
+    return category
 
-        bmi_value = (weight * 0.45) / ((((height_ft*12) + height_in) * 0.025) ** 2)
-        category = self.bmi_category_calculation(bmi_value)
-        output = "Your BMI is " + str(round(bmi_value, 1)) + " and you are " + category + "."
-        
-        return output
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    bmi = None
+    category = None
+    if request.method == 'POST':
+        weight = float(request.form['weight'])
+        height = float(request.form['height'])
+        if weight > 0 and height > 0:
+            bmi = calculate_bmi_value(weight, height)
+            category = calculate_bmi_category(bmi)
+    return render_template('index.html', bmi=bmi, category=category)
 
-def main():
-     bmi = BMI()
-     print(bmi.bmi_value_calculation())
-
-if __name__ == "__main__":
-     main()
+if __name__ == '__main__':
+    app.run(debug=True)
